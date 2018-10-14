cc3f40bf2449d1ed50aa9caf2a78ace12692029a
Sarah George <sngeorge27@gmail.com>
2018-10-13 23:59:55 -0400

    Initial Commit

---
+++ Flashcard App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard App
+//
+//  Created by Sarah George on 10/13/18.
+//  Copyright © 2018 Sarah George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
