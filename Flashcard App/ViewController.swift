b8bce4ceefca30da7a116ea29f4ac9c2b100341c
Sarah George <sngeorge27@gmail.com>
2018-11-02 20:40:36 -0400

    updated code for adding card

--- Flashcard App/ViewController.swift
+++ Flashcard App/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,18 @@ class ViewController: UIViewController {
 
     @IBAction func didTapFlascard(_ sender: Any) {
         frontLabel.isHidden = true
+    }
+    func updateFlashcard(question: String, answer: String)  {
+        frontLabel.text = question
+        backLabel.text = answer
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
     }
 }
 
